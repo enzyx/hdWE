5e3dd838b5898b986fa332b4e144c90fb516440a
Fabian Zeller <Fabian Zeller>
2015-01-10 18:02:16 +0100

    update

---
+++ Trajectory.py
@@ -0,0 +1,7 @@
+class Trajectory()
+
+	def __init__(self,probability,parent_bin,parent_trajectory):
+	
+		self.probability = probability
+		self.parent_bin	   	= parent_bin
+		self.parent_trajectory	= parent_trajectory
