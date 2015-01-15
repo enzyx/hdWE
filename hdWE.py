8ba4896bb05d8ea4f3cd2602b332d76d4f550b3a
Manuel Luitz <manuel.luitz@gmail.com>
2015-01-15 16:49:43 +0100

    Cleaning up
    Moved argparser in extra file

--- hdWE.py
+++ hdWE.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-import argparse
+import arg_parser as args
 import numpy
 import initiate
 from iteration import Iteration
@@ -9,41 +9,6 @@ from bin import Bin
 from segment import Segment
 from logger import Logger
 
-#############################
-# parsing the commandline
-parser = argparse.ArgumentParser(description=
-    'hdWE is a hyperdimensional weighted ensemble simulation implementation.')
-parser.add_argument('-d', '--dir', type=str, 
-                    dest="work_dir", required=True, metavar="DIR",
-                    help="The working direcory")
-parser.add_argument('-c', '--conf', type=str, dest="input_md_conf", 
-                    required=True, metavar="FILE",
-                    help="The starting structure file")
-parser.add_argument('--segments-per-bin', type=int, dest="segments_per_bin", 
-                    metavar="10", default=10, nargs='?',
-                    help="Number of trajectories per bin")
-parser.add_argument('--iterations', type=int, dest="max_iterations", 
-                    metavar="50", default=50, nargs='?',
-                    help="Maximum number of iterations")
-parser.add_argument('--threshold', type=float, dest="coordinate_threshold", 
-                    metavar="0.1", default=0.1, nargs='?',
-                    help="Defines the minimal RMSD of a trajectory to all other bins "
-                         "after which a new bin is created")
-parser.add_argument('--minimal-probability', type=float, dest="input_minimal_probability", 
-                    metavar="0.01", default=0.01, nargs='?',
-                    help="Minimal probability a trajectory must have to"
-                    " allow forking a new bin")
-parser.add_argument('--debug', dest="debug", action="store_true",
-                    default=False, help="Turn debugging on")
-parser_mdgroup = parser.add_mutually_exclusive_group(required=True)
-parser_mdgroup.add_argument("--amber", dest="amber", action="store_true",
-                    default=False)
-parser_mdgroup.add_argument("--gromacs", dest="gromacs", action="store_true",
-                    default=False)
-args = parser.parse_args()
-#############################
-
-#############################
 # The global list of arrays
 iterations = []
 
@@ -53,14 +18,15 @@ logger = Logger("logfile.log")
 initiate.prepare(args.work_dir, starting_structure="", override="", debug=args.debug)
 iterations.append(initiate.create_initial_iteration(args.segments_per_bin))
 
+# Check MD suite
 if(args.amber):
     from amber_module import MD_module
+    md_module = MD_module(args.work_dir, args.input_md_conf, debug=args.debug)
 if(args.gromacs):
     print("Sorry, support for gromacs is not implemented yet.")
     sys.exit(-1)
 
-md_module = MD_module(args.work_dir, args.input_md_conf, debug=args.debug)
-
+# Logger load
 logger.read_iterations()
 
 # Loop
