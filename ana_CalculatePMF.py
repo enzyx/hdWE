abdcbbe56ac5ee6962db119e4b0893e16cb247fd
Rainer Bomblies <r.bomblies@tum.de>
2015-04-16 10:39:53 +0200

    CalculatePMF adapted to new logging
    also direct plotting
    and plotting of segment positions
    possible

--- ana_CalculatePMF.py
+++ ana_CalculatePMF.py
@@ -4,6 +4,7 @@ from the data of a hdWE run.
 """
 from __future__ import print_function
 import numpy
+import matplotlib.pyplot as plt  
 import constants
 import sys
 import segment
@@ -12,6 +13,24 @@ from math import log
 from amber_module import MD_module
 import argparse  
 
+### classes ### 
+class SegmentData(object):
+    """
+    a container for resulting data of a segment
+    """
+    def __init__(self, iteration_index, bin_index, coordinate):
+        self.iteration_index = iteration_index
+        self.bin_index = bin_index
+        self.coordinate = coordinate
+        
+    def getIterationId(self):
+        return self.iteration_index
+              
+    def getBinId(self):
+        return self.bin_index
+              
+    def getCoordinate(self):
+        return self.coordinate
 
 ###### Parse command line ###### 
 parser =argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -19,9 +38,9 @@ parser =argparse.ArgumentParser(description=__doc__, formatter_class=argparse.Ra
 parser.add_argument('-c', '--conf', dest="input_md_conf", 
                     type=str, required=True,
                     help="MD-Software configuration file")
-parser.add_argument('-l', '--log', type=str, dest="logfile", 
-                    default="logfile.log", metavar="FILE",
-                    help="The logfile for reading and writing")
+parser.add_argument('-l', '--log', type=str, dest="logdir", 
+                    default="log", metavar="DIR",
+                    help="The log directory")
 parser.add_argument('-b', '--first_it', dest="first_iteration",
                     type=int, default=0,
                     help="First iteration to use for PMF calculation.")                    
@@ -31,14 +50,30 @@ parser.add_argument('-e', '--last_it', dest="last_iteration",
 parser.add_argument('-o', '--output', dest="output_path", 
                     type=str, default='ana_calculatePMF.output',
                     help="Output filename")  
+parser.add_argument('--op', '--plotfile', dest="output_plot", 
+                    type=str, default='ana_calculatePMF.pdf',
+                    help="Filename for output plot.")  
 parser.add_argument('-N', '--number_of_bins', dest="number_of_bins",
                     type=int, default=100, 
                     help="Number of bins used to calculate the probability histogram.")  
 parser.add_argument('-i', '--cpptraj_lines_file', dest="cpptraj_lines_file_path", 
                     type=str,   required=True,
                     help="File containig cpptraj syntax that defines the reaction coordinate.")
+parser.add_argument('-p', '--plot', dest="plot", action="store_true",
+                    default=False, help="plot result directly to screen.")    
+parser.add_argument('-r', '--reference', nargs='?', default=False, 
+                    dest="reference", metavar="FILE", const="reference.dat",
+                    help="The (optional) reference file for comparison with cMD.")  
+parser.add_argument('--rc', '--reference_column', nargs='?', default=False, 
+                    dest="ref_column", metavar="INT", const=2, type=int,
+                    help="The (optional) reference file for comparison with cMD.")                      
 parser.add_argument('-y', dest="use_trajectory", action="store_true",
-                    default=False, help="use conformations from the trajectories, not only the end conformation")                    
+                    default=False, help="use conformations from the trajectories,"\
+                                        "not only the end conformation")                                                   
+parser.add_argument('--segments', dest="plot_segments", action="store_true",
+                    default=False, help="plot final segment positions of last iteration.")
+parser.add_argument('--bin-references', dest="plot_bin_references", action="store_true",
+                    default=False, help="plot bin reference positions")                                        
                     
 # Initialize
 print('\033[1mCalculating PMF\033[0m (Free Energy is given in kcal/mol at 298K).')      
@@ -46,9 +81,8 @@ args = parser.parse_args()
 md_module = MD_module(args.input_md_conf, debug=False)
 
 #get the actual Iteration from logger module
-logger = Logger(args.logfile, APPEND = True)
-iterations = logger.loadIterations(args.first_iteration, args.last_iteration)
-logger.close()
+logger = Logger(args.logdir)
+iterations = logger.loadIterations(begin=args.first_iteration, end=args.last_iteration)
 
 print(str(len(iterations)))
 
@@ -65,6 +99,9 @@ cpptraj_lines_file.close()
 
 #Calculate the coordinate values and store them together with
 #the trajectory probability into coordinates 
+
+segments = []
+references = []
 coordinates         = numpy.zeros([0,2])
 coordinates_tmp     = numpy.zeros([1,2])
 coordinates_tmp_tmp = numpy.zeros([1,2])
@@ -77,9 +114,12 @@ for iteration_loop in iterations:
         sys.stdout.write(' Processing iteration ' + str(iteration_loop.getId()).zfill(5) +  \
                          ' / ' + str(first_it).zfill(5) + '-' + str(last_it).zfill(5) + \
                          ', Bin ' +  str(bin_loop.getId()+1).zfill(5) + '/ ' + str(iteration_loop.getNumberOfBins()).zfill(5) + '\r' )
-        sys.stdout.flush()  
+        sys.stdout.flush()
         for segment_loop in bin_loop:
-            coordinates_tmp_tmp = md_module.ana_calcCoordinateOfSegment(segment_loop, cpptraj_lines, args.use_trajectory)
+            coordinate = md_module.ana_calcCoordinateOfSegment(segment_loop, cpptraj_lines, use_trajectory = args.use_trajectory)
+            if iteration_loop.getId() == iterations[-1].getId():
+                segments.append(SegmentData(iteration_loop.getId(), bin_loop.getId(), coordinate))
+            coordinates_tmp_tmp = coordinate
             probability_tmp = segment_loop.getProbability()
             for i in range(0,len(coordinates_tmp_tmp)):
                 coordinates_tmp[0,0] = coordinates_tmp_tmp[i]
@@ -89,7 +129,7 @@ for iteration_loop in iterations:
 #Calculate the coordinate values of the bin reference structures
 bin_coordinates     = numpy.zeros([0,1])
 bin_coordinates_tmp = numpy.zeros([1,1])
- 
+
 for bin_loop in iterations[-1]:
     #create temporary segment to pass to md_module, because the bin reference segment
     #is not necessarilly within the loaded range of iterations
@@ -97,6 +137,8 @@ for bin_loop in iterations[-1]:
                   iteration_id = bin_loop.getReferenceIterationId(),
                   bin_id       = bin_loop.getReferenceBinId(),
                   segment_id   = bin_loop.getReferenceSegmentId() )
+    coordinate = md_module.ana_calcCoordinateOfSegment(segment_tmp, cpptraj_lines, False)
+    references.append(SegmentData(iteration_loop.getId(), bin_loop.getId(), coordinate))
     bin_coordinates_tmp[0] = md_module.ana_calcCoordinateOfSegment(segment_tmp, cpptraj_lines, False)
     bin_coordinates        = numpy.append(bin_coordinates, bin_coordinates_tmp, axis=0) 
 
@@ -137,6 +179,55 @@ for i in range(0,args.number_of_bins):
 #Save PMF to file
 header_line = 'Coordinate Value, Free Energy, Probability, Values per hist-Bin, hdWE Bins per hist-Bin'
 numpy.savetxt(args.output_path, hist, header = header_line)
+print('\n PMF data written to: ' + args.output_path) 
 
-print('\n Output written to: ' + args.output_path)   
+# Plotting
+segment_colors = ["Blue", "Red", "Green", "Orange", "c", "m", "y"]
+seg_step = 0.3
+f, ax = plt.subplots(1,1)
+ax.grid()
+
+# load reference (cMD) data
+if args.reference:
+    ref_data = numpy.transpose(numpy.loadtxt(args.reference))
+    ax.plot(ref_data[0], ref_data[args.ref_column-1], label=args.reference)
+
+ax.plot(hist[:,0], hist[:,1], label=args.logdir)
+
+if args.plot_segments:
+    for bin_id in range(iterations[-1].getNumberOfBins()):
+        seg_x = []
+        seg_y = []
+        for segment in segments:
+            if segment.getBinId() == bin_id:
+                seg_x.append(segment.getCoordinate())
+                seg_y.append(-seg_step - seg_step*segment.getBinId())
+                
+            if segment.getBinId() > bin_id:
+                break
+        ax.scatter(seg_x, 
+                seg_y,
+                marker="s",
+                color=segment_colors[bin_id%len(segment_colors)])
+
+if args.plot_bin_references:
+    bin_ref_x = []
+    bin_ref_y = []    
+    for index, bin_loop in enumerate(iterations[-1]):
+        bin_ref_x.append(references[index].getCoordinate())
+        bin_ref_y.append(-1)
+        bin_color = 1.0*bin_loop.getId()/iterations[-1].getNumberOfBins()
+    cmap = plt.get_cmap("coolwarm")
+    ax.scatter(bin_ref_x, 
+               bin_ref_y,
+               marker="s")               
+
+ax.legend()
+if args.plot:
+    plt.show() 
+else:
+    mpl.pyplot.savefig(args.output_plot, bbox_inches='tight', transparent=True)
+    print('\n Output written to: ' + args.output_plot)
+
+   
 
