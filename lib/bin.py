6a0ae7deacf6e186f05b828c6bad423102a03d6a
Fabian <fabian.zeller@mytum.de>
2015-07-08 15:58:10 +0200

    removed start bin artefacts from hdWE code

--- lib/bin.py
+++ lib/bin.py
@@ -162,18 +162,7 @@ class Bin(object):
         for index in range(len(self.segments)):
             self.segments[index].setSegmentId(index)
     
-    def isStartStateBin(self):
-        """
-        @return Is this bin a start state bin
-        """
-        return self.is_start_bin
-    
-    def isEndStateBin(self):
-        """
-        @return Is this bin a end state bin
-        """
-        return self.is_end_bin
-    
+   
     def getCoordinateIds(self):
         """
         @return list of coordinate ids
@@ -277,20 +266,7 @@ class Bin(object):
         """
         return self.target_number_of_segments
     
-    def testBinState(self, STATES):
-        """
-        tests if coordinate ids of bin are in STATES (e.g. start or end state)
-        @return boolean
-        """
-        if type(STATES) == bool:
-            return STATES
-        
-        for state in STATES:
-            # compare element wise
-            if self.getCoordinateIds() == state:
-                return True
-        return False
-    
+   
     def __iter__(self):
         """
         Defines the class as iterable.
