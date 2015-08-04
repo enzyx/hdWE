c9c1cf57c8a9bce8cbda6c1b4f8eaaca84eff07e
Manuel Luitz <manuel.luitz@gmail.com>
2015-08-04 17:57:54 +0200

    New tool ana_IterationInfo
    A new handy tool to extract live information from
    iterations. This will be extended in the future.

---
+++ ana_IterationInfo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+"""
+Modify an existing simulation by skipping initial bins
+"""
+from __future__ import print_function
+from lib.logger import Logger
+import argparse
+import sys
+
+###### Parse command line ###### 
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+parser.add_argument('-l', '--log', type=str, dest="logdir", 
+                    metavar="FOLDER", required=True,
+                    help="The log directory for reading")
+parser.add_argument('-i', '--iteration', dest="iteration_index",
+                    type=int, default=-1,
+                    help="Iteration which is modified")
+
+args = parser.parse_args()
+logger = Logger(args.logdir)
+try:
+    iteration = logger.loadIteration(args.iteration_index)
+except IOError:
+    print("Could not find file for iteration {}!".format(args.iteration_index))
+    sys.exit(-1)
+
+for this_bin in iteration:
+    print(" Bin: {: 4d} Segments: {: 4d} p: {: 5.4e}\tCoord ids: {}".format(this_bin.getId(),
+                                               this_bin.getNumberOfSegments(),
+                                               this_bin.getProbability(), 
+                                               this_bin.getCoordinateIds()))
