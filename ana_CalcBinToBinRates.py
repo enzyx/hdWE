d6a88fa359389feb11aa3678932e6e561c13a978
Rainer Bomblies <r.bomblies@tum.de>
2015-08-07 15:06:41 +0200

    bugfix ana_CalcBinToBinRates
    sorting of transition matrix in multiple
    dimensions works

--- ana_CalcBinToBinRates.py
+++ ana_CalcBinToBinRates.py
@@ -3,16 +3,44 @@
 Return the bin to bin transition matrix or the the rate matrix
 """
 from __future__ import print_function
-import numpy
+import numpy as np
 from lib.logger import Logger
 import argparse
 import lib.analysis_operations as analysis_operations
 import sys
 
 ###### Function ######
-def printMatrix(matrix):
+
+def sortMatrix(matrix, order):
+    """
+    sorts rows and columns of a matrix according
+    to given order. order is interpreted as order[source_index] = target_index
+    """
+    # if order has wrong dimension or 
+    if len(order) != len(matrix):
+        raise Exception('wrong order dimension given')
+    # or matrix isn't square I can't work
+    elif not all (len (row) == len (matrix) for row in matrix):
+        raise Exception('can\'t sort non-square matrix')
+    
+    sort_matrix = np.zeros((len(order),len(order)), dtype=int)
+    for source_index, target_index in enumerate(order):
+        sort_matrix[target_index, source_index] = 1
+
+         
+    new_matrix = np.dot(sort_matrix, matrix)
+    new_matrix = np.dot(new_matrix, np.transpose(sort_matrix))        
+    return new_matrix
+        
+
+def printMatrix(matrix, coord_ids = []):
     digits = str(len(str(matrix.max())) + 1)
+    if len(coord_ids) != 0:
+        sys.stdout.write("coordinate id of |   transition\nsort dimension   |     matrix\n")
     for i in range(len(matrix)):
+        if len(coord_ids) != 0:
+            sys.stdout.write("{:2d} ".format(int(coord_ids[i])))
+            sys.stdout.write("|")
         for j in range(len(matrix[0])):
             if i==j:
                 sformat = "\033[0;31m{: "+digits+"d}\033[0m"
@@ -43,15 +71,16 @@ parser.add_argument('-o', '--output', dest="outfile", metavar="OUTPUT",
                     help="Write the matrix to this file")
 parser.add_argument('-s', '--sort', dest='sort_dimension', 
                     metavar='INT', default=0, type=int, 
-                    help='Dimension index along which to sort bins.')
+                    help='Dimension index along which to sort bins. -1 does not sort.')
 
 args = parser.parse_args()
 logger = Logger(args.logdir)
 iterations = logger.loadIterations(args.first_iteration, args.last_iteration)
 
 N = iterations[-1].getNumberOfBins()
-DIMENSION = args.sort_dimension
-transition_matrix = numpy.zeros((N,N), int)
+DIMENSIONS = len(iterations[-1].bins[0].getCoordinateIds())
+SORT_DIMENSION = args.sort_dimension
+transition_matrix = np.zeros((N,N), int)
 
 if args.bin_to_bin_transitions:
     for iteration in iterations:
@@ -61,58 +90,37 @@ if args.bin_to_bin_transitions:
                 transition_matrix[iteration.bins[segment.getParentBinId()].getId(), 
                                   iteration.bins[segment.getBinId()].getId()]      += 1
 
-    if args.sort_dimension != -1:                       
-        # sort along dimension
-        sort_matrix = []
-        largest_coordinate_id = 0
-        for this_bin in iterations[-1]:
-            if this_bin.getCoordinateIds()[DIMENSION] > largest_coordinate_id:
-                largest_coordinate_id = this_bin.getCoordinateIds()[DIMENSION]
-        sort_dimensions = numpy.max([(largest_coordinate_id+1), N])
-        large_sort_matrix = numpy.zeros((sort_dimensions, N), int)
-        for this_bin in iterations[-1]:
-            this_coord = this_bin.getCoordinateIds()[DIMENSION]
-            large_sort_matrix[this_coord, this_bin.getId()] = 1
-        b_got_values = False
-        # cut all empty bins before first sampled one, 
-        for line in large_sort_matrix:
-            if numpy.sum(line) != 0:
-                b_got_values = True
-            if not b_got_values:
-                continue    
-            sort_matrix.append(line)
-        sort_matrix = numpy.asarray(sort_matrix, dtype=int)
-        # find lines full of zeros and remember their index
-        unfound_coord_ids = []
-        for line_index, line in enumerate(sort_matrix):
-            if numpy.sum(line) == 0:
-                unfound_coord_ids.append(line_index)
-    
-         
-        transition_matrix = numpy.dot(sort_matrix, transition_matrix)
-        transition_matrix = numpy.dot(transition_matrix, numpy.transpose(sort_matrix))
-    
-        # set values of unfound bins to -1
-        for coord_id in unfound_coord_ids:
-            transition_matrix[coord_id, :] = -1
+    if args.sort_dimension != -1:    
+        # collect coordinate ids of bins        
+        bin_list = []
+        for this_bin in iteration:
+            # save bin_id as additional element to know sorting order later
+            coord_id = this_bin.getCoordinateIds()[:].tolist()
+            coord_id.append(this_bin.getId())
+            bin_list.append(np.asarray(coord_id))
 
-    if not args.outfile: 
-        printMatrix( transition_matrix )
+        # sort in order of dimensions
+        sort_dimension_order = [SORT_DIMENSION] \
+                               + np.arange(SORT_DIMENSION).tolist() \
+                               + np.arange(SORT_DIMENSION+1, DIMENSIONS).tolist()
+        sorted_bin_list = np.asarray(sorted(bin_list, 
+                                            key=lambda element: tuple([element[i] for i in sort_dimension_order])))
+        sorting_order = sorted_bin_list[:,-1]
+        
+        transition_matrix = sortMatrix(transition_matrix, sorting_order)
+        coordinate_ids_for_plot = sorted_bin_list[:,SORT_DIMENSION]
     else:
-        numpy.savetxt(args.outfile, transition_matrix, fmt='% 4d')
+        # to preserve non-sorting functionality          
+        coordinate_ids_for_plot = []
+    if not args.outfile: 
+        printMatrix( transition_matrix, coordinate_ids_for_plot)
+    else:
+        np.savetxt(args.outfile, transition_matrix, fmt='% 4d')
 else:
     rateMatrix = analysis_operations.meanRateMatrix(iterations)
     if not args.outfile:
         print(rateMatrix)
     else:
-        numpy.savetxt(args.outfile, rateMatrix, fmt='%.2f')
+        np.savetxt(args.outfile, rateMatrix, fmt='%.2f')
 
-# start_start_rate = 0.0
-# for i in range(len(rateMatrix)):
-#     for j in range(len(rateMatrix)):
-#         if iterations[-1].bins[j].isStartStateBin() == True:
-#             start_start_rate += rateMatrix[i,j] * iterations[-1].bins[i].getProbability()
-# 
-# print (start_start_rate)
-        
 
