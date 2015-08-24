88110a189ff02ecb86f47de4756523193ab5dab8
Fabian <fabian.zeller@mytum.de>
2015-08-24 17:42:36 +0200

    sorting function in iteration info

--- ana_IterationInfo.py
+++ ana_IterationInfo.py
@@ -16,11 +16,15 @@ parser.add_argument('-l', '--log', type=str, dest="logdir",
 parser.add_argument('-i', '--iteration', dest="iteration_index",
                     type=int, default=-1,
                     help="Iteration which is modified")
+parser.add_argument('--sort', dest="sorting",
+                    type=bool, default=True,
+                    help="Flag for sorting of bins according to coordinate Ids")
 prev_data = parser.add_mutually_exclusive_group()
 prev_data.add_argument('-s', '--segments', dest="print_segments", default=False, action="store_true",
                     help="Print segment info")
 prev_data.add_argument('-p', '--parent-segments', dest="parent_segments", default=False, action="store_true",
                     help="Print segment parent info")
+
 
 args = parser.parse_args()
 logger = Logger(args.logdir)
@@ -29,6 +33,17 @@ try:
 except IOError:
     print("Could not find file for iteration {}!".format(args.iteration_index))
     sys.exit(-1)
+
+
+if args.sorting:
+    def bin_compare(x, y):
+        for dimension in range(0, len(x.coordinate_ids)):
+            diff = (x.coordinate_ids[dimension] - y.coordinate_ids[dimension])
+            if (x.coordinate_ids[dimension] - y.coordinate_ids[dimension]) != 0:
+                return diff
+
+    iteration.bins = sorted(iteration.bins, cmp = bin_compare)
+
 
 for this_bin in iteration:
     print(" Bin: {:<4d} Segments: {:<4d} p: {:<5.4e}  Coord ids: {} Active: {}".format(this_bin.getId(),
